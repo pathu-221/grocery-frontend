74e4f03ff3797f7123e4d5a6d61a2dfa49d709fa
pathu-221 <prathamaggarwal23@gmail.com>
2023-09-25 12:23:07 +0530

    update product rerviews

--- src/components/ProductDetailPage/ProductReviews.tsx
+++ src/components/ProductDetailPage/ProductReviews.tsx
@@ -95,7 +95,7 @@ const ProductReviews: FC<ProductReviewsProps> = ({}) => {
 										<figure className="comment-media">
 											<a href="#">
 												<img
-													src={`https://api.dicebear.com/7.x/initials/svg?seed=${review.user.first_name}`}
+													src={`https://api.dicebear.com/7.x/initials/svg?seed=$d{review.user.first_name}`}
 													width="100"
 													height="100"
 													alt="avatar"
